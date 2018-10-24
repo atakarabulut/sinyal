8944708419ec5cefa275fdf0e699d11a9cc5ff56
atakarabulut <44411594+atakarabulut@users.noreply.github.com>
2018-10-24 23:32:45 +0300

    Add files via upload

---
+++ a_ve_b_secenegi.m
@@ -0,0 +1,34 @@
+clear all
+close all
+gun = 4 % 1 sene 10 gün
+a=1/4; % belirlediðim alfa1 deðeri 
+b=3/5; % belirlediðim alfa1 deðeri
+d=1/2 % belirlediðim alfa2 deðeri
+e=3/5 % belirlediðim alfa2 deðeri
+g=1/4 % belirlediðim alfa3 deðeri
+h=3/4 % belirlediðim alfa3 deðeri
+% günlük artýþ miktarým
+title('Ilac Miktarý')
+xlabel('Gunler') % 
+ylabel('Alýnan toplam doz')
+for i=1:gun %loop
+    c(i)=a+b 
+    a=b 
+    b=c(i)
+    hold on
+    stem(i,b)
+    for z=1:gun
+        f(i)=d+e 
+        d=e 
+        e=f(i)
+        stem(z,e)
+        for y=1:gun
+            l(i)=g+h 
+            g=h 
+            h=l(i)
+            stem(y,g)
+        end
+    end
+end
+
+% genel mantýk : c[n]=a(alfa)+B(beta)*c[n-1]
